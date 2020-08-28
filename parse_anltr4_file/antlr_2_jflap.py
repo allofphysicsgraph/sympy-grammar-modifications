56b185c7c77d0edaf24be74792f00f918c9b87ca
mike <mike@lcd-outlet.com>
2020-08-28 03:40:49 +0000

    basic transitions

--- parse_anltr4_file/antlr_2_jflap.py
+++ parse_anltr4_file/antlr_2_jflap.py
@@ -1,9 +1,8 @@
 import re
-from random import shuffle
+from random import randint
 
 with open("/home/user/LaTeX.g4") as f:
     latex_g4 = f.read()
-
 
 labels = re.findall("(.*?):.*?;\n", latex_g4, re.DOTALL)
 labels = [x.strip() for x in labels if x if "//" not in str(x) and " " not in str(x)]
@@ -11,12 +10,12 @@ terminal_group_labels = [x for x in labels if not re.findall("[a-z]", x)]
 non_terminals = [x for x in labels if x not in terminal_group_labels]
 
 mapping = {}
+map_name_to_id = {}
 counter = 0
 parsed_g4 = re.findall("(.*?):(.*?);\n", latex_g4, re.DOTALL)
 
 for line in parsed_g4[2:]:
     value = line[1].strip().replace("'", "")
-
     key = line[0].strip()
     values = [x.strip() for x in re.split("\s+\|\s+|\(|\)|\n", value) if x]
     mapping[key] = values
@@ -35,10 +34,10 @@ def print_transitions(src, dst, edge_string):
     t = f"""<transition>
             <from>{src}</from>
             <to>{dst}</to>
-            <read>{edge_string}>/read>
+            <read>{edge_string}</read>
             </transition>
             """
-    print(t)
+    return t
 
 
 header = """<?xml version="1.0" encoding="UTF-8" standalone="no"?><!--Created with JFLAP 7.1.--><structure>
@@ -55,21 +54,41 @@ def write_jflap_files(file_name):
 f = write_jflap_files("test.jff")
 f.write(header)
 f.write("\n")
-mapping_keys = list(mapping.keys())
-shuffle(mapping_keys)
 current_x_coord_lside = 50
 current_y_coord_lside = -300
 current_y_coord_rside = 50
-for idx, k in enumerate(mapping.keys()):
-    if k in terminal_group_labels:
-        s = print_state(idx, k, current_x_coord_lside + 300, current_y_coord_rside)
+seen = set()
+for idx, k in enumerate(set(mapping.keys())):
+    if k in terminal_group_labels and k not in seen:
+        map_name_to_id[k] = idx
+        s = print_state(
+            idx,
+            k,
+            current_x_coord_lside + 600 + randint(-200, 200),
+            current_y_coord_rside,
+        )
         current_y_coord_rside -= 50
-    else:
+        seen.add(k)
+        f.write(s)
+        f.write("\n")
+        continue
+    elif k in non_terminals and k not in terminal_group_labels and k not in seen:
+        map_name_to_id[k] = idx
         s = print_state(idx, k, current_x_coord_lside, current_y_coord_lside)
         current_y_coord_lside -= 50
-    f.write(s)
-    f.write("\n")
+        seen.add(k)
+        f.write(s)
+        f.write("\n")
+        continue
 
+for key in mapping.keys():
+    if key in non_terminals:
+        match = [x for x in mapping[key] if x in terminal_group_labels]
+        if match:
+            for dst in [x for x in match]:
+                s = print_transitions(map_name_to_id[key], map_name_to_id[dst], dst)
+                f.write(s)
+                f.write("\n")
 f.write("</automaton>")
 f.write("\n")
 f.write("</structure>")

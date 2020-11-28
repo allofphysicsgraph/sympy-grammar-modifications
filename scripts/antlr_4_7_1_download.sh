48c797f0b102d477a4ba89881dc0e754784d2092
mike <mike@lcd-outlet.com>
2020-11-28 03:27:05 +0000

    verify download

--- scripts/antlr_4_7_1_download.sh
+++ scripts/antlr_4_7_1_download.sh
@@ -1,4 +1,11 @@
-if [[ ! -f antlr-4.7.1-complete.jar ]];
+if [[ ! -f /usr/local/lib/antlr-4.7.1-complete.jar ]];
 then
-	curl -O https://www.antlr.org/download/antlr-4.7.1-complete.jar
+	cd /tmp/
+	curl -O https://www.antlr.org/download/antlr-4.7.1-complete.jar 
+	checksum="$(md5sum /tmp/antlr-4.7.1-complete.jar |grep -c 8084572679b2b2a1c126aa6e62e48ce5)"
+	echo $checksum
+	if [[ $checksum -eq 1 ]];
+	then
+		sudo rsync -axr /tmp/antlr-4.7.1-complete.jar /usr/local/lib/ --progress --remove-source-files;
+	fi
 fi

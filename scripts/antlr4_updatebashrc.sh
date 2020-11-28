adceaf7c2bf1c5851740b4d24069e4561db2b227
mike <mike@lcd-outlet.com>
2020-11-28 03:06:20 +0000

    typo

--- scripts/antlr4_updatebashrc.sh
+++ scripts/antlr4_updatebashrc.sh
@@ -1,3 +1,4 @@
-echo -e export CLASSPATH=\".:/usr/local/lib/antlr-4.7.1-complete.jar.jar:\$CLASSPATH\" >> ~/.bashrc
-echo -e alias grun=\'java -Xmx500M -cp \"/usr/local/lib/antlr-4.7.1-complete.jar.jar:\$CLASSPATH\" org.antlr.v4.gui.TestRig\'  >> ~/.bashrc
-echo -e alias grun=\'java -Xmx500M -cp \"/usr/local/lib/antlr-4.7.1-complete.jar.jar:\$CLASSPATH\" org.antlr.v4.gui.TestRig\'  >> ~/.bashrc
+echo -e export CLASSPATH=\".:/usr/local/lib/antlr-4.7.1-complete.jar:\$CLASSPATH\" >> ~/.bashrc
+echo -e  alias antlr4=\'java -Xmx500M -cp \"/usr/local/lib/antlr-4.7.1-complete.jar:\$CLASSPATH\" org.antlr.v4.Tool\' >> ~/.bashrc
+echo -e alias grun=\'java -Xmx500M -cp \"/usr/local/lib/antlr-4.7.1-complete.jar:\$CLASSPATH\" org.antlr.v4.gui.TestRig\'  >> ~/.bashrc
+
